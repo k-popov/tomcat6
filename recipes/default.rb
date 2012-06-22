f115bcd5d171f941d2774cb054543aa19f4e317d
Kirill Popov <kpopov@griddynamics.com>
2012-06-22 13:41:54 +0400

    Renamed init.d script from 'tomcat' to 'tomcat6'

--- recipes/default.rb
+++ recipes/default.rb
@@ -237,13 +237,13 @@ ruby_block "remove_standard_webapps" do
 end
 
 # Create startup script in /etc/init.d/ directory
-template "/etc/init.d/tomcat" do
-    source "tomcat_init.erb"
+template "/etc/init.d/tomcat6" do
+    source "tomcat6_init.erb"
     mode "0755"
 end
 
 # enable and start the service
-service "tomcat" do
+service "tomcat6" do
     supports :restart => true
     action [ :enable, :start ]
 end
